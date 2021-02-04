22ab879adbd7bb6c2282ca02726f70f1cf074394
Connie Ho <connie3@ualberta.ca>
2021-02-04 20:17:31 +0000

    eqobjects recursion

--- eqObjects.js
+++ eqObjects.js
@@ -1,4 +1,3 @@
-
 const assertEqual = function(actual, expected) {
   if (actual === expected) {
     console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`);
@@ -12,11 +11,13 @@ const eqArrays = function(arr1, arr2) {
   
   if (n !== arr2.length) return false;
   
-
   for (let i  = 0; i < n; i ++) {
-    if (arr1[i] !== arr2[i]) return false;
+    if (Array.isArray(arr1[i]) && Array.isArray(arr2[i])) {
+      return eqArrays(arr1[i], arr2[i]);
+    } else if (arr1[i] !== arr2[i]) {
+      return false;
+    }
   }
-
   return true;
 };
 
@@ -32,18 +33,26 @@ const eqObjects = function(object1, object2) {
   for (const key of keys1) {
     if (Array.isArray(object1[key]) && Array.isArray(object2[key])) {
       return eqArrays(object1[key], object2[key]);
-    }  else if (object1[key] !== object2[key]) {
+    } else if (typeof object1[key] === 'object' && typeof object2[key] === 'object') {
+      return eqObjects(object1[key], object2[key]);
+    } else if (object1[key] !== object2[key]) {
       return false;
     }
   }
-
   return true;
-
 };
+      
 
-const cd = { c: "1", d: ["2", 3] };
-const dc = { d: ["2", 3], c: "1" };
-assertEqual(eqObjects(cd, dc), true); // => true
 
-const cd2 = { c: "1", d: ["2", 3, 4] };
-assertEqual(eqObjects(cd, cd2), false); // => false
+
+// const cd = { c: "1", d: ["2", 3] };
+// const dc = { d: ["2", 3], c: "1" };
+// assertEqual(eqObjects(cd, dc), true); // => true
+
+// const cd2 = { c: "1", d: ["2", 3, 4] };
+// assertEqual(eqObjects(cd, cd2), false); // => false
+
+// assertEqual(eqObjects({ a: { z: 1 }, b: 2 }, { a: { z: 1 }, b: 2 }), true) // => true
+assertEqual(eqObjects({ a: { z: [1,2,[3,4]] }, b: 2 }, { a: { z: [1,2,[3,4]] }, b: 2 }), true); // => true
+// assertEqual(eqObjects({ a: { y: 0, z: 1 }, b: 2 }, { a: { z: 1 }, b: 2 }), false) // => false
+// assertEqual(eqObjects({ a: { y: 0, z: 1 }, b: 2 }, { a: 1, b: 2 }), false) // => false
