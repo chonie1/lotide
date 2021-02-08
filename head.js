13b4cd72f867c997f38c9a381554a8e4e6b57453
Connie Ho <connie3@ualberta.ca>
2021-02-08 17:17:05 +0000

    refactoring head

--- head.js
+++ head.js
@@ -1,10 +1,4 @@
-const assertEqual = function(actual, expected) {
-  if (actual === expected) {
-    console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`);
-  } else {
-    console.log(`❌❌❌ Assertion Failed: ${actual} !== ${expected}`);
-  }
-};
+// const assertEqual = require('./assertEqual')
 
 const head = function(arr) {
   if (!arr.length) return [];
@@ -12,8 +6,5 @@ const head = function(arr) {
   return arr[0];
 };
 
-//Tests
-assertEqual(head([], 5));
-assertEqual(head([5]), 5);
-assertEqual(head([5,6,7]), 5);
-assertEqual(head(["Hello", "Lighthouse", "Labs"]), "Hello");
+module.exports = head;
+
