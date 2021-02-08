494f4874ce0b8c18ab0b560c04f7f9cb50841f7e
Connie Ho <connie3@ualberta.ca>
2021-02-08 17:28:24 +0000

    eqArrays test

--- test/eqArraysTest.js
+++ test/eqArraysTest.js
@@ -1,9 +1,7 @@
-const assertEqual = require('../assertEqual');
+const assertArraysEqual = require('../assertArraysEqual');
 const eqArrays = require('../eqArrays');
 
-assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true); // => should PASS
-assertEqual(eqArrays([1, 2, 3], [3, 2, 1]), false); // => should not PASS
-assertEqual(eqArrays(["1", "2", "3"], ["1", "2", "3"]), true); // => should PASS
-assertEqual(eqArrays(["1", "2", "3"], ["1", "2", 3]), false); // => should not PASS
-assertEqual(eqArrays([[2, 3], [4]], [[2, 3], [4]]), true);
-assertEqual(eqArrays([[2, 3], [4]], [[2, 3], 4]), false);
+assertArraysEqual([1, 2, 3], [1, 2, 3]);
+assertArraysEqual([1, 2, 3], [3, 2, 1]);
+assertArraysEqual(["1", "2", "3"], ["1", "2", "3"]);
+assertArraysEqual(["1", "2", "3"], ["1", "2", 3]);
